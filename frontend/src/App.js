d54e266b371cd72a9a5efff800ab9612ce29238f
hiimvincent <vincentjohnson1908@gmail.com>
2023-07-17 12:38:01 -0700

    Project setup and create frontend footer/header components

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h1>First Case</h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
