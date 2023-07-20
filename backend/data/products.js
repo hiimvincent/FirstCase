1b6ac962c3009fad652a1dcf6def03537268886c
hiimvincent <vincentjohnson1908@gmail.com>
2023-07-20 10:49:33 -0700

    added data seeder

---
+++ backend/data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: "Cat and Dog Cute Cartoon Phone Case",
+    image: "/images/animal_phone_case.jpg",
+    description:
+      "Adorable cat and dog cartoon phone case. Great for any occasion. Available for all iPhone models.",
+    brand: "Cute Cases Inc.",
+    category: "Pet & Animal",
+    price: 19.99,
+    countInStock: 19,
+    rating: 4.52,
+    numReviews: 129,
+  },
+  {
+    name: "Slay Chips GenZ Unfunny Phone Case",
+    image: "/images/chips_phone_case.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Funny",
+    price: 23.99,
+    countInStock: 71,
+    rating: 5.00,
+    numReviews: 88,
+  },
+  {
+    name: "Ugly Frog Smiling Funny Phone Case",
+    image: "/images/frog_phone_case.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Funny",
+    price: 18.49,
+    countInStock: 58,
+    rating: 4.81,
+    numReviews: 102,
+  },
+  {
+    name: "Silly Melon Dad Joke Phone Case",
+    image: "/images/melon_phone_case.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Food & Snack",
+    price: 19.99,
+    countInStock: 191,
+    rating: 5.00,
+    numReviews: 71,
+  },
+  {
+    name: "Totoro Studio Ghibli Anime Character Case",
+    image: "/images/totoro_phone_case.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Character",
+    price: 12.99,
+    countInStock: 37,
+    rating: 4.28,
+    numReviews: 238,
+  },
+  {
+    name: "Adorable Capybara Pizza Meme Phone Case",
+    image: "/images/capybara_phone_case.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Pet & Animal",
+    price: 27.49,
+    countInStock: 4,
+    rating: 4.53,
+    numReviews: 160,
+  },
+  {
+    name: "Haku Dragon Spirited Away Phone Case",
+    image: "/images/haku_phone_case.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Character",
+    price: 21.99,
+    countInStock: 52,
+    rating: 4.93,
+    numReviews: 94,
+  },
+  {
+    name: "Kiki's Delivery Service Studio Ghibli Case",
+    image: "/images/kiki_phone_case.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    brand: "Lorem ipsum",
+    category: "Character",
+    price: 19.99,
+    countInStock: 41,
+    rating: 4.88,
+    numReviews: 151,
+  },
+];
+
+export default products;
